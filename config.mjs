7333b94cabc1fce41ceca79fb7e339b33820a1a3
clabel <clabel_18492@hotmail.com>
2025-10-10 23:31:12 -0600

    preguntas

--- config.mjs
+++ config.mjs
@@ -3,111 +3,197 @@ export const WEBSOCKET_SERVER_PORT = 5505
 
 const QUIZZ_CONFIG = {
   password: "PASSWORD",
-  subject: "Adobe",
+  subject: "CONEXIÓN BÍBLICA",
   questions: [
     {
-      question: "¿Quién fue el primer hombre creado?",
+      question: "¿Para quién fue escrito el libro de Lucas?",
       answers: [
-        "Moisés",
-        "Abraham",
-        "Adán",
-        "Noé",
+        "Judíos",
+        "Nosotros",
+        "Teófilo",
+        "Romanos"
       ],
       solution: 2,
       cooldown: 5,
       time: 15,
     },
     {
-      question: "¿Cuál fue el primer milagro de Jesús según el Evangelio de Juan?",
+      question: "¿De quién era descendiente Elisabet?",
       answers: [
-        "Sanar a un ciego", 
-        "Caminar sobre el agua",
-        "Resucitar a Lázaro",
-        "Convertir el agua en vino"
+        "Aarón",
+        "Levi",
+        "Judá",
+        "Moisés"
       ],
-      image:
-        "https://images.unsplash.com/photo-1626785774573-4b799315345d?q=80&w=500&auto=webp",
-      solution: 3,
-      cooldown: 5,
-      time: 15,
-    },
-    {
-      question: "¿Cuántos libros tiene la Biblia en total?",
-      answers: ["66", "43", "22", "73"],
       solution: 0,
       cooldown: 5,
       time: 15,
     },
     {
-      question: "¿Quién construyó el arca para salvarse del diluvio?",
+      question: "¿A quién le dieron la noticia del nacimiento de Juan?",
       answers: [
-        "Moisés","David","Noé","Abraham"
+        "Zacarías", 
+        "Abías", 
+        "Juan", 
+        "Elizabet"
       ],
-      solution: 2,
-      cooldown: 5,
-      time: 15,
-    },
-    {
-      question: "¿Dónde recibió Moisés los Diez Mandamientos?",
-      answers: [
-        "Monte Sinaí",
-        "Monte Carmelo",
-        "Monte de los Olivos",
-        "Monte Ararat"
-      ],
-      image:
-        "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?q=80&w=500&auto=webp",
       solution: 0,
       cooldown: 5,
       time: 15,
     },
     {
-      question: "¿Qué apóstol negó a Jesús tres veces?",
+      question: "¿Cómo se llama el ángel que habló con María?",
       answers: [
+        "Miguel",
         "Juan",
-        "Tomás",
-        "Judas",
-        "Pedro"
+        "Gabriel",
+        "Daniel",
       ],
-      image:
-        "https://images.unsplash.com/photo-1435348773030-a1d74f568bc2?q=80&w=500&auto=webp",
-      solution: 3,
+      solution: 2,
       cooldown: 5,
       time: 15,
     },
     {
-      question: "¿Cuál de estos libros está en el Antiguo Testamento?",
+      question: "¿De qué forma comunicó Zacarías el nombre de su hijo, cuando le preguntaron con señas cómo se llamaría?",
       answers: [
-        "Romanos",
-        "Génesis",
-        "Hechos",
-        "Filipenses",
+        "Con muecas",
+        "Con el lenguaje de señas", 
+        "Lo escribió en una tablita",
+        "Hablando", 
+      ],
+      solution: 2,
+      cooldown: 8,
+      time: 15,
+    },
+    {
+      question: "¿Quién dió el edicto de que todo el mundo fuera empadronado?",
+      answers: [
+        "Cirenio", 
+        "Augusto César", 
+        "Pilato",
+        "Herodes" 
       ],
       solution: 1,
       cooldown: 5,
       time: 15,
     },
     {
-      question: "¿Qué mujer en la Biblia se convirtió en estatua de sal?",
+      question: "¿De dónde a dónde viajaron José y María para ser empadronados?",
       answers: [
-        "Ester",
-        "María Magdalena",
-        "Raquel",
-        "La esposa de Lot",
+        "De Galilea a Nazaret",
+        "De Judea a Belén",
+        "De Nazaret a Belén",
+        "De Galilea a Belén",
       ],
       solution: 3,
       cooldown: 5,
       time: 15,
     },
     {
-      question: "¿Quién fue tragado por un gran pez por desobedecer a Dios?",
+      question: "¿A los cuántos días fue circuncidado Jesús?",
       answers: [
-        "Elías",
-        "Isaías",
-        "Jonás",
-        "Pablo",
+        "7 días",
+        "8 días",
+        "10 días",
+        "40 días"
+      ],
+      solution: 1,
+      cooldown: 5,
+      time: 15,
+    },
+    {
+      question: "¿Cuántos meses de embarazo tenía su prima de María, cuando el ángel habló con ella?",
+      answers: [
+        "2 meses",
+        "4 meses",
+        "6 meses",
+        "8 meses"
+      ],
+      solution: 2,
+      cooldown: 8,
+      time: 15,
+    },
+    {
+      question: "¿Cómo se llamaba el hombre al que el Espíritu Santo le dijo que no vería la muerte hasta que viera al ungido del Señor?",
+      answers: [
+        "José", 
+        "Felipe",
+        "Fanuel", 
+        "Simeón",
       ],
       solution: 3,
+      cooldown: 8,
+      time: 15,
+    },
+    {
+      question: "¿Hacia cuanto tiempo la profetisa Ana era viuda?",
+      answers: [
+        "82 años",
+        "84 años",
+        "86 años",
+        "89 años"
+      ],
+      solution: 1,
+      cooldown: 5,
+      time: 15,
+    },
+    {
+      question: "¿Quienes eran los sumos sacerdotes cuando vino palabra de Dios a Juan en el desierto?",
+      answers: [
+        "Timoteo y Nicodemo",
+        "Anás y Caifás",
+        "Zacarías y Ananías",
+        "Tiberio y Felipe"
+      ],
+      solution: 1,
+      cooldown: 8,
+      time: 15,
+    },
+    {
+      question: "¿Que les contestó Juan a los publicanos que vinieron a él para ser bautizados, cuando le dijeron: que haremos?",
+      answers: [
+        "No hagáis extorsión a nadie",
+        "Bienvenidos, el reino de los cielos es para todos",
+        "El que tiene dos túnicas, dé al que no tiene",
+        "No exijáis más de lo que os está ordenado."
+      ],
+      solution: 3,
+      cooldown: 8,
+      time: 18,
+    },
+    {
+      question: "¿Por causa de quien reprendió Juan a Herodes el tetrarca?",
+      answers: [
+        "Felipe",
+        "Herodías",
+        "De la gente",
+        "María"
+      ],
+      solution: 1,
+      cooldown: 5,
+      time: 15,
+    },
+    {
+      question: "¿De cuantos años mas o menos empezó Jesús su ministerio?",
+      answers: [
+        "24 años",
+        "28 años",
+        "30 años",
+        "32 años"
+      ],
+      solution: 2,
+      cooldown: 5,
+      time: 15,
+    },
+     {
+      question: "¿Hijo de quien era José el padre de Jesús?",
+      answers: [
+        "Eli",
+        "Amós",
+        "Eliezer",
+        "Elias"
+      ],
+      solution: 0,
       cooldown: 5,
       time: 15,
     },
